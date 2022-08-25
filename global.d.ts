3b2490007f555f26c0ecd22a7ed8fe070d913f41
Davis Okioma <davis.okioma@savannahinformatics.com>
2022-08-25 03:34:24 +0300

    chore: initial frontend project structure

---
+++ global.d.ts
@@ -0,0 +1,3 @@
+declare module "@tailwindcss/typography";
+declare module "@tailwindcss/line-clamp";
+declare module "@tailwindcss/aspect-ratio";
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			container: {
+				center: true,
+				padding: "2rem",
+			},
+		},
+	},
+	plugins: [
+		require("@tailwindcss/typography"),
+		require("@tailwindcss/forms"),
+		require("@tailwindcss/line-clamp"),
+		require("@tailwindcss/aspect-ratio"),
+	],
+};
